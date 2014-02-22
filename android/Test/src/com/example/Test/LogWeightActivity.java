448ffe3ffdc380be6eb66f423ca63aa5ef446774
Oliver Carabia <oliver.carabia@gmail.com>
2014-02-22 22:22:11 +0100

    create activities

---
+++ android/Test/src/com/example/Test/LogWeightActivity.java
@@ -0,0 +1,14 @@
+package com.example.Test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Oliver on 22/02/14.
+ */
+public class LogWeightActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logweight);
+    }
+}
--- android/Test/src/com/example/Test/MyActivity.java
+++ android/Test/src/com/example/Test/MyActivity.java
@@ -1,7 +1,9 @@
 package com.example.Test;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MyActivity extends Activity {
@@ -13,5 +15,17 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+    }
+
+    public void openLogWeight(View view)
+    {
+        Intent intent = new Intent(this, LogWeightActivity.class);
+        startActivity(intent);
+    }
+
+    public void openViewHistory(View view)
+    {
+        Intent intent = new Intent(this, ViewHistoryActivity.class);
+        startActivity(intent);
     }
 }
