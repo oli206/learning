3ef533ee5a1237872ed987abad064ad84679fdb7
Oli <oliver.carabia@gmail.com>
2014-02-22 20:59:53 +0100

    create android project

---
+++ android/Test/src/com/example/Test/MyActivity.java
@@ -0,0 +1,17 @@
+package com.example.Test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
---
+++ tomcattest/src/example/HelloWorldClient.java
@@ -0,0 +1,8 @@
+package example;/**
+ * Created by Oliver on 8/02/14.
+ */
+public class HelloWorldClient {
+  public static void main(String[] argv) {
+
+  }
+}
